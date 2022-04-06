32c5bcba0d092c435ee0d5fdc48d328e9b094bd2
root <root@example.com>
2022-04-06 23:27:20 +0200

    auto

--- src/pyrc/customize.py
+++ src/pyrc/customize.py
@@ -7,6 +7,10 @@ Examples:
 >>> from pathlib import Path  # doctest: +SKIP
 >>> (Path(site.__file__).parent / "sitecustomize.py").write_text("from pyrc.customize import *")  # doctest: +SKIP
 """
+
+# TODO: tests pendientes y comando actualizar el sitecustomize.py, ver ue tengo si solo importo pyrc.startup en consola o si al final
+#  es lo mismo que hacer una funcion y llamar en __init__.py asi no tengo que importar en startup.py todo lo de env...
+
 import importlib.util
 from . import typings
 from importlib.util import spec_from_file_location
