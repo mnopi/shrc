3d374e8e805affc213d0d8860c067c6d52292136
root <root@example.com>
2022-09-24 08:21:05 +0200

    git-sync: D  etc/ipython/ipython_config.py D  etc/ipython/startup/prompt.py D  etc/ipython/startup/sys_path.py D  etc/ipython/startup/userwarning.py D  src/shrc/.idea/dictionaries D  src/shrc/.idea/inspectionProfiles/Project_Default.xml D  src/shrc/.idea/inspectionProfiles/profiles_settings.xml D  src/shrc/.idea/misc.xml D  src/shrc/.idea/modules.xml D  src/shrc/.idea/scopes D  src/shrc/.idea/shrc.iml D  src/shrc/.idea/vagrant.xml D  src/shrc/.idea/vcs.xml D  src/shrc/.idea/workspace.xml D  src/shrc/__init__.py D  src/shrc/alias.py D  src/shrc/color.py D  src/shrc/constants.py D  src/shrc/customize.py D  src/shrc/env/__init__.py D  src/shrc/env/default.py D  src/shrc/env/misc.py D  src/shrc/env/osrelease.py D  src/shrc/env/secrets.py D  src/shrc/env/system.py D  src/shrc/env/utils.py D  src/shrc/jetbrains.py D  src/shrc/startup.py D  src/shrc/utils.py D  src/shrc/variables.py D  tests/helpers/helper.bash D  tests/pyrc/test_aioclone.py D  tests/pyrc/test_pyrc_cli.py D  tests/pyrc/test_pyrc_imports.py

--- etc/ipython/startup/userwarning.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-"""
-Shrc UserWarning Module
-"""
-import warnings
-
-warnings.filterwarnings("ignore", category=UserWarning)
--- tests/helpers/helper.bash
+++
@@ -1,4 +0,0 @@
-# shellcheck shell=bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")"/../.. || return
-source ./libexec/bats.bash
